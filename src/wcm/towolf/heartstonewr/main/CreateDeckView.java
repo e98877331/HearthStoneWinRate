1064ea543555fe6526ca554c3bfd609233d367f5
ChengYan Yang <e98877331@gmail.com>
2013-11-07 18:15:34 +0800

    add ui for createView in mainActivity

--- src/wcm/towolf/heartstonewr/main/CreateDeckView.java
+++ src/wcm/towolf/heartstonewr/main/CreateDeckView.java
@@ -1,45 +1,50 @@
 package wcm.towolf.heartstonewr.main;
 
 import itri.u9lab.towolf.ratiofixer.RatioFixer;
+import wcm.towolf.hearthstonewr.R;
 import wcm.towolf.hearthstonewr.view.HeroChooseView;
 import wcm.towolf.hearthstonewr.view.HeroChooseView.ClickCallBack;
 import android.content.Context;
-import android.graphics.Color;
 import android.text.InputFilter;
 import android.view.Gravity;
-import android.view.KeyEvent;
-import android.view.View;
 import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethod;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class CreateDeckView extends RelativeLayout{
 
 	RatioFixer mRF;
+ImageView mBGView;	
+	
 	TextView mTitle;
 	EditText mName;
 	HeroChooseView mChooseView;
 	public CreateDeckView(final Context context,RatioFixer rf) {
 		super(context);
 		// TODO Auto-generated constructor stub
-		this.setBackgroundColor(Color.GRAY);
+		//this.setBackgroundResource(R.drawable.main_create_bg);
 		
 		mRF = rf;
 		
+		mBGView = new ImageView(context);
+		mBGView.setImageResource(R.drawable.main_create_bg);
+		mBGView.setScaleType(ScaleType.FIT_XY);
+		this.addView(mBGView,mRF.getLayoutParam(650, 1150, 0, 0));
+		
 		mTitle = new TextView(context);
 		mTitle.setGravity(Gravity.CENTER);
-		mTitle.setText("Add New Deck");
+		mTitle.setText(R.string.main_create_view_title);
 		mTitle.setTextSize(mRF.getRealValue(30));
-		this.addView(mTitle,mRF.getLayoutParam(630, 100, 0, 0));
+		this.addView(mTitle,mRF.getLayoutParam(630, 100, 10, 10));
 		
 		mName = new EditText(context);
 		mName.setId(1);
 //		mName.setBackgroundColor(Color.RED);
 		mName.setGravity(Gravity.CENTER); 
-		mName.setText("dfadff");
+		mName.setText(R.string.main_create_view_name);
 		mName.setFilters( new InputFilter[] {
 				   new InputFilter.LengthFilter(12)});
 		mName.setSingleLine();
@@ -78,12 +83,12 @@ public class CreateDeckView extends RelativeLayout{
 //		        return false;
 //		    }
 //		});
-		this.addView(mName,mRF.getLayoutParam(630, 100, 0, 100));
+		this.addView(mName,mRF.getLayoutParam(630, 100, 10, 110));
 		
 		
 		
 		mChooseView = new HeroChooseView(context, rf);
-		this.addView(mChooseView,mRF.getLayoutParam(630,930,0,200));
+		this.addView(mChooseView,mRF.getLayoutParam(630,930,10,210));
 		
 		
 	}

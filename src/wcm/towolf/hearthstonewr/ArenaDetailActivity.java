2e03f4ac9441ebf080a38a427b72d84be1d3e0f1
Cheng-yan Yang <e98877331@gmail.com>
2014-01-29 03:47:38 +0800

    Add ArenaHeroDetailData

--- src/wcm/towolf/hearthstonewr/ArenaDetailActivity.java
+++ src/wcm/towolf/hearthstonewr/ArenaDetailActivity.java
@@ -1,27 +1,41 @@
 package wcm.towolf.hearthstonewr;
 
-import com.google.analytics.tracking.android.EasyTracker;
-
 import itri.u9lab.towolf.ratiofixer.RatioRelativeLayout;
+
+import java.util.ArrayList;
+
+import wcm.towolf.hearthstonewr.model.ArenaEventDataProvider;
+import wcm.towolf.hearthstonewr.model.datatype.RoleType;
+import wcm.towolf.hearthstonewr.model.datatype.arena.ArenaHeroDetailData;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.graphics.LightingColorFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 
+import com.google.analytics.tracking.android.EasyTracker;
+
 public class ArenaDetailActivity extends Activity {
 
+	final static String TAG = "ArenaDetailAcitvity";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		
+		
+   CyyTest();
+	    
+		
 		RatioRelativeLayout rl = new RatioRelativeLayout(this);
+	
+		
 		rl.setBackgroundResource(R.drawable.main_bg);
 		TextView tv = new TextView(this);
 		tv.setText("Coming \nS       n");
@@ -62,5 +76,21 @@ public class ArenaDetailActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onStop();
 		EasyTracker.getInstance(this).activityStop(this); 
+	}
+	
+	public void CyyTest()
+	{
+	    long tt = System.currentTimeMillis();
+		ArenaEventDataProvider provider = new ArenaEventDataProvider();
+		ArrayList<ArenaHeroDetailData> herodata =  provider.getAllArenaHeroData();
+	    for(ArenaHeroDetailData hero: herodata)
+	    {
+	    	Log.e(TAG,"HeroType: "+ RoleType.getDebugString(hero.getRoleType(),this) + " Win:"+hero.getWins()+" Total:"+hero.getTotal());
+	    	for(ArenaHeroDetailData.ArenaVSHeroData vsd : hero.getVsHeroList())
+	    	{
+	    	   Log.e(TAG,"vs:" + RoleType.getDebugString(vsd.getVSRoleType(),this)+ " Win: " + vsd.getWins()+" Total:"+ vsd.getTotal());
+	    	}
+	    }
+		Log.e(TAG,"Run time:" +Long.toString(System.currentTimeMillis() - tt));
 	}
 }
---
+++ src/wcm/towolf/hearthstonewr/model/datatype/RoleGameList.java
@@ -0,0 +1,40 @@
+package wcm.towolf.hearthstonewr.model.datatype;
+
+import java.util.ArrayList;
+
+public class RoleGameList {
+	
+	public ArrayList<RoleGame> mGames;
+	public RoleGameList()
+	{
+		mGames = new ArrayList<RoleGame>();
+	}
+
+	public void addGame(RoleGame game)
+	{
+		mGames.add(game);
+	}
+	
+	public int getWins()
+	{
+		int winCount = 0;
+		for(int i = 0; i< mGames.size();i++)
+		{
+			if(mGames.get(i).getIsWin())
+				winCount++;
+		}
+		return winCount;
+	}
+	
+	public int getTotal()
+	{
+	   return mGames.size();
+	}
+	
+	public int getLoses()
+	{
+		return getTotal() - getWins();
+	}
+	
+	
+}

c2c220c77a0064402dcf9af6e643d21884ce3f69
ChengYan Yang <e98877331@gmail.com>
2014-02-04 16:46:14 +0800

    add ga tracking for arena event start

--- src/wcm/towolf/hearthstonewr/ArenaActivity.java
+++ src/wcm/towolf/hearthstonewr/ArenaActivity.java
@@ -14,7 +14,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
@@ -27,111 +26,90 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.MapBuilder;
 
 public class ArenaActivity extends Activity {
 	ArenaView mView;
 
 	TopPanel mTopPanel;
 	NewEventPanel mNewEventPanel;
-    ListView mListView;
-    MainAdapter mAdapter;
-    
-    ArrayList<ArenaEventData> mData;
-	
-    
-	
+	ListView mListView;
+	MainAdapter mAdapter;
+
+	ArrayList<ArenaEventData> mData;
+
 	ArenaEventDataProvider mProvider;
-	
+
 	HeroChooseDialog mHCD;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		
-		/*final ArenaEventDataProvider provider = new ArenaEventDataProvider();
 
-		final Random rd = new Random();
-
-		LinearLayout rl = new LinearLayout(this);
-
-		Button testBtn = new Button(this);
-		testBtn.setText("testbun");
-		testBtn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-
-				provider.addEvent(rd.nextInt(7));
-				provider.addEvent(rd.nextInt(7));
-				provider.addEvent(rd.nextInt(7));
-				provider.addEvent(rd.nextInt(7));
-
-			}
-		});
-
-		Button testBtn2 = new Button(this);
-		testBtn2.setText("testbtn2");
-		testBtn2.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				provider.addGame(rd.nextInt(4), rd.nextInt(7), rd.nextBoolean());
-			}
-		});
-
-		Button testBtn3 = new Button(this);
-		testBtn3.setText("testbtn3");
-		testBtn3.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				ArrayList<ArenaEventData> data = provider
-						.getAllArenaEventData();
-				int a = 5;
-				a = a + 1;
-			}
-		});
-
-		rl.addView(testBtn);
-		rl.addView(testBtn2);
-		rl.addView(testBtn3);
-		//*/
+		/*
+		 * final ArenaEventDataProvider provider = new ArenaEventDataProvider();
+		 * 
+		 * final Random rd = new Random();
+		 * 
+		 * LinearLayout rl = new LinearLayout(this);
+		 * 
+		 * Button testBtn = new Button(this); testBtn.setText("testbun");
+		 * testBtn.setOnClickListener(new OnClickListener() {
+		 * 
+		 * @Override public void onClick(View arg0) { // TODO Auto-generated
+		 * method stub
+		 * 
+		 * provider.addEvent(rd.nextInt(7)); provider.addEvent(rd.nextInt(7));
+		 * provider.addEvent(rd.nextInt(7)); provider.addEvent(rd.nextInt(7));
+		 * 
+		 * } });
+		 * 
+		 * Button testBtn2 = new Button(this); testBtn2.setText("testbtn2");
+		 * testBtn2.setOnClickListener(new OnClickListener() {
+		 * 
+		 * @Override public void onClick(View v) { // TODO Auto-generated method
+		 * stub provider.addGame(rd.nextInt(4), rd.nextInt(7),
+		 * rd.nextBoolean()); } });
+		 * 
+		 * Button testBtn3 = new Button(this); testBtn3.setText("testbtn3");
+		 * testBtn3.setOnClickListener(new OnClickListener() {
+		 * 
+		 * @Override public void onClick(View v) { // TODO Auto-generated method
+		 * stub ArrayList<ArenaEventData> data = provider
+		 * .getAllArenaEventData(); int a = 5; a = a + 1; } });
+		 * 
+		 * rl.addView(testBtn); rl.addView(testBtn2); rl.addView(testBtn3); //
+		 */
 
 		mProvider = new ArenaEventDataProvider();
-		
-		
-		mView = new ArenaView(this);
-		
 
+		mView = new ArenaView(this);
 
 		mTopPanel = mView.topPanel;
 		mTopPanel.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				Intent i = new Intent(ArenaActivity.this, ArenaDetailActivity.class);
+				Intent i = new Intent(ArenaActivity.this,
+						ArenaDetailActivity.class);
 				// i.putExtra("RoleData",mAdapter.getItem(arg2));
 				startActivity(i);
 
 			}
 		});
 
-		
 		mListView = mView.listView;
-        //mListView.setAdapter(new MainAdapter());
+		// mListView.setAdapter(new MainAdapter());
 		reloadListData();
-		
+
 		setNewEventPanel();
-		
+
 		// for onLongClick delete item purpose
 		registerForContextMenu(mListView);
-		
-		
+
 		mView.setToContentView(this);
 	}
 
@@ -139,23 +117,24 @@ public class ArenaActivity extends Activity {
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
-		
+
 	}
-	
+
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		
+
 		EasyTracker.getInstance(this).activityStart(this);
 	}
+
 	@Override
 	protected void onStop() {
 		// TODO Auto-generated method stub
 		super.onStop();
-		EasyTracker.getInstance(this).activityStop(this); 
+		EasyTracker.getInstance(this).activityStop(this);
 	}
-	
+
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
@@ -180,9 +159,9 @@ public class ArenaActivity extends Activity {
 		// String menuItemName = menuItems[menuItemIndex];
 		// String listItemName = "listitem Name";//Countries[info.position];
 		if (menuItemIndex == 0) {
-			
-			mData.get(mData.size() - info.position -1).delete();
-			//deleteRoleDataFromList(info.position);
+
+			mData.get(mData.size() - info.position - 1).delete();
+			// deleteRoleDataFromList(info.position);
 			reloadListData();
 
 		}
@@ -191,99 +170,114 @@ public class ArenaActivity extends Activity {
 		// menuItemIndex, info.position), Toast.LENGTH_LONG).show();
 		return true;
 	}
-	
+
 	private void setNewEventPanel() {
-		
-		mHCD = new HeroChooseDialog(ArenaActivity.this,
-				mView.getRatioFixer());
+
+		mHCD = new HeroChooseDialog(ArenaActivity.this, mView.getRatioFixer());
 		mHCD.setInnerTitle(R.string.arena_which_hero);
 		mHCD.setItemClickListener(new ClickCallBack() {
 
 			@Override
 			public void run(int roleType) {
 				// TODO Auto-generated method stub
+
+				GALog(roleType);
 				
-				
-              long eventID =  mProvider.addEvent(roleType);
-              ArenaEventData aed = mProvider.getEvent((int)eventID);
-              mNewEventPanel.setData(aed);                      
-              reloadListData();
-              mHCD.dismiss();
-              
-            //this only set start event btn to invisible
+				long eventID = mProvider.addEvent(roleType);
+				ArenaEventData aed = mProvider.getEvent((int) eventID);
+				mNewEventPanel.setData(aed);
+				reloadListData();
+				mHCD.dismiss();
+
+				// this only set start event btn to invisible
 				mNewEventPanel.startEvent();
-              
+
 			}
 		});
-		
+
 		mNewEventPanel = mView.newEventPanel;
 		mNewEventPanel.setStartEventOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				if(!mHCD.isShowing());
-				mHCD.showAndAdjustWindow();
+				if (!mHCD.isShowing()) {
+				
+					mHCD.showAndAdjustWindow();
+				}
 			}
 		});
-		
+
 		mNewEventPanel.setWinClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				mNewEventPanel.win();
-				//reloadListData();
+				// reloadListData();
 			}
 		});
-		
+
 		mNewEventPanel.setLoseClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				mNewEventPanel.lose();
-				//reloadListData();
+				// reloadListData();
 			}
 		});
-		
-		
+
 		initNewEventPanel();
-	
+
 	}
-	
-	private void initNewEventPanel()
-	{
-		//no any data yet
-		if(mData.size()<1)
+
+	private void initNewEventPanel() {
+		// no any data yet
+		if (mData.size() < 1)
 			return;
-		
-		ArenaEventData lastData = mData.get(mData.size()-1);
-		
-		if(!lastData.isFinished())
-		{
+
+		ArenaEventData lastData = mData.get(mData.size() - 1);
+
+		if (!lastData.isFinished()) {
 			mNewEventPanel.startEvent();
 			mNewEventPanel.setData(lastData);
 		}
-		
+
 	}
-	
-	public  void reloadListData() {
-		
-	//	long tt = System.currentTimeMillis();
+
+	public void reloadListData() {
+
+		// long tt = System.currentTimeMillis();
 
 		mData = mProvider.getAllArenaEventData();
-   mTopPanel.update(mData);
+		mTopPanel.update(mData);
 		if (mAdapter == null) {
 			mAdapter = new MainAdapter();
 			mListView.setAdapter(mAdapter);
 		}
 		mAdapter.notifyDataSetChanged();
 
-	//	Log.e("ArenaActivity", "CYYRUNTimeReload: "+Long.toString(System.currentTimeMillis() - tt));
+		// Log.e("ArenaActivity",
+		// "CYYRUNTimeReload: "+Long.toString(System.currentTimeMillis() - tt));
 	}
-	
-	
+
+	private void GALog(int roleType) {
+
+		EasyTracker easyTracker = EasyTracker.getInstance(this);
+
+		// MapBuilder.createEvent().build() returns a Map of event fields and
+		// values
+		// that are set and sent with the hit.
+		easyTracker.send(MapBuilder.createEvent("ui_action", // Event category
+																// (required)
+				"button_press", // Event action (required)
+				"event_start_button", // Event label
+				(long)roleType) // Event value
+				.build());
+
+	}
+
 	public class MainAdapter extends BaseAdapter {
 		// ArrayList<RoleData> mData;
 		public MainAdapter() {
@@ -300,7 +294,7 @@ public class ArenaActivity extends Activity {
 		public ArenaEventData getItem(int arg0) {
 			// TODO Auto-generated method stub
 
-				return mData.get(arg0);
+			return mData.get(arg0);
 		}
 
 		@Override
@@ -313,32 +307,30 @@ public class ArenaActivity extends Activity {
 		public View getView(int index, View convertView, ViewGroup arg2) {
 			// TODO Auto-generated method stub
 
+			if (convertView == null
+					|| !(convertView instanceof ArenaViewListItem)) {
+				convertView = new ArenaViewListItem(ArenaActivity.this,
+						mView.getRatioFixer());
+			}
 
-				if (convertView == null || !(convertView instanceof ArenaViewListItem)) {
-					convertView = new ArenaViewListItem(ArenaActivity.this,
-							mView.getRatioFixer());
-				}
+			ArenaEventData rd = mData.get(mData.size() - index - 1);
 
-			     ArenaEventData rd = mData.get(mData.size() - index -1);
+			// Log.e("ArenaActivity","Event ID = "+Integer.toString(rd.eventID));
 
-			   // Log.e("ArenaActivity","Event ID = "+Integer.toString(rd.eventID));
-			     
-				((ArenaViewListItem) convertView).setData(rd);
-				if(rd.isFinished())
-					convertView.setAlpha(1f);
-				else
-				{
-					//not to show the view
-					View view = new View(ArenaActivity.this);
-					return view;
-				}
-//					convertView.setAlpha(0.5f);
-					//convertView.setVisibility(View.GONE);
+			((ArenaViewListItem) convertView).setData(rd);
+			if (rd.isFinished())
+				convertView.setAlpha(1f);
+			else {
+				// not to show the view
+				View view = new View(ArenaActivity.this);
+				return view;
+			}
+			// convertView.setAlpha(0.5f);
+			// convertView.setVisibility(View.GONE);
 
-			
 			return convertView;
 		}
 
 	}
-	
+
 }
